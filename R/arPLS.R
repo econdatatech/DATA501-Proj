e8bf93d378c81a8cdabde2afe396107de10a892c
Corvin Idler <corvin.idler@gmail.com>
2024-09-03 14:37:28 +1100

    main algo

---
+++ R/arPLS.R
@@ -0,0 +1,161 @@
+#' @title asymmetrically reweighted penalized least squares
+#'
+#' @author Corvin Idler
+#' @description
+#' Baseline correction using asymmetrically reweighted penalized least
+#' squares smoothing (Baek et al. 2015).
+#'
+#' @details
+#' The algorithm iteratively estimates a spectral baseline curve by updating a
+#' weight vector by means of a generalized logistic function that focuses the
+#' estimation efforts on regions where the baseline and the signal are close to
+#' each other
+#'
+#' @references
+#' Baek, S.-J., Park, A., Ahn, Y.-J., and Choo, J. (2015). Baseline correction
+#' using asymmetrically reweighted penalized least squares smoothing.
+#' Analyst, 140:250–257.
+#'
+#' @param y Numeric vector representing the spectrum.
+#' @param lambda Smoothing parameter (default: 1e6).
+#' @param p Asymmetry parameter (default: 1e-6).
+#' @param max_iter Maximum number of iterations (default: 50).
+#' @param verbose Boolean to print intermediary outputs (default: FALSE).
+#' @return object of class arPLSresult:
+#' \itemize{
+#'   \item \code{rawinput}: The original spectrum fed into the algorithm.
+#'   \item \code{lambda}: The lambda parameter fed into the algorithm.
+#'   \item \code{ratio}: The ratio stopping parameter fed into the algorithm.
+#'   \item \code{max_iter}: The maximum iteration stopping parameter fed into the algorithm.
+#'   \item \code{baseline}: The fitted spectral baseline.
+#'   \item \code{last_iter}: The number of iterations the algorithm did before stopping.
+#'   \item \code{last_ratio}: The last value of the ratio stopping criterium before stopping.
+#' }
+#'
+#' @examples
+#' y <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+#' baseline <- baseline_correction(y)
+#' @export baseline_correction
+
+
+
+new_arPLSresult <- function(rawinput = numeric(), lambda = 1e6, ratio=1e-6, max_iter=50,
+                            baseline=numeric(),last_iter=int(),last_ratio=double()) {
+  object<-list(rawinput=rawinput, lambda=lambda,ratio=ratio,max_iter=max_iter,baseline=baseline,
+            last_iter=last_iter,last_ratio=last_ratio)
+  attr(object,"class") <- "arPLSresult"
+  object
+}
+
+plot.arPLSresult<- function(x){
+  plot(x$rawinput,main="arPLS baseline estimation",ylab="Measurements")
+  lines(x$baseline,col='red')
+
+}
+
+summary.arPLSresult<- function(x){
+  print(paste("The lambda parmeter value used was: ",x$lambda))
+  print(paste("The ratio parameter value used was: ",x$ratio))
+  print(paste("The max_iter parameter value used was: ",x$max_iter))
+  print(paste("The alogrithm stopped after the following number or iterations: ",x$last_iter))
+  print(paste("The last weight ratio value was: ",x$last_ratio))
+  if(x$last_iter==x$max_iter){
+    print("It appears that the algorithm stopped because the maximum number of iterations was reached")
+  }
+  if(x$last_ratio<x$ratio){
+    print("It appears that the algorithm stopped because the change in weights per iteration fell below the ratio threshold")
+  }
+}
+
+library(Rcpp)
+library(RcppArmadillo)
+Rcpp::cppFunction("arma::mat armaInv(const arma::mat & x) { return arma::inv(x); }", depends="RcppArmadillo")
+
+
+# default values from page 253 of original publication
+baseline_correction <- function(y, lambda = 1e6, ratio = 1e-6, max_iter = 50,verbose=FALSE) {
+#input validation
+  if (missing(y)) {
+    stop("Missing input data. You need to provide an input vector for y")
+  }
+  if (!is.numeric(y)) {
+    stop("The input data must be of type numeric.")
+  }
+  if (anyNA(y)) {
+    stop("The input spectrum contains NA values. You need to correct your input data so it doesn't contain NAs")
+  }
+  if (sum(is.infinite(y)) >= 1) {
+    stop("The input spectrum contains Inf values. You need to correct your input data so it doesn't contain Infs")
+  }
+  if (length(y) <= 2) {
+    stop("The input spectrum must contain at least 3 values")
+  }
+  if (length(y[y<0]) > 0) {
+    stop("The input spectrum must contain only positive values")
+  }
+  if (!is.numeric(lambda) || length(lambda) != 1 || sum(is.infinite(lambda)) >= 1 || lambda <0 ) {
+    stop("The parameter 'lambda' must be a single valid numeric value.
+         NA, Inf or negative values are not valid.")
+  }
+  if (!is.numeric(ratio) || length(ratio) != 1 || sum(is.infinite(ratio)) >= 1   || lambda <0 ) {
+    stop("The parameter 'ratio' must be a single valid numeric value.
+         NA, Inf or negative values are not valid.")
+  }
+  if (!is.numeric(max_iter) || length(max_iter) != 1 || sum(is.infinite(max_iter)) >= 1   || max_iter <0 || max_iter%%1!=0 ) {
+    stop("The parameter 'max_iter' must be a single valid integer value.
+         NA, Inf or negative values or fractions and decimals are not valid.")
+  }
+  if (!is.logical(verbose) || length(verbose) != 1  ) {
+    stop("The parameter 'verbose' must be a single boolean value. TRUE or FALSE are the only valid inputs.")
+  }
+
+  n <- length(y)
+  D <- diff(diag(n), differences = 2)
+  H<-lambda*(t(D) %*% D)
+  w <- rep(1, n)
+  wold <- w
+  arPLSresult<-new_arPLSresult(rawinput=y,lambda,ratio,max_iter,baseline=y,0,1.0)
+  for (i in 1:max_iter) {
+    if(verbose){print(paste("Iteration: ",i))}
+    arPLSresult$last_iter<-i
+
+    W <- diag(as.numeric(w))
+    z <- solve(W + H) %*% (W %*% y)
+    #z <- armaInv(W + H) %*% (W %*% y)
+    arPLSresult$baseline<-z
+    d=y-z
+    dneg=d[d<0]
+
+    #Not sure what to do when there are no or only 1 element with d<0
+    #Doesn't allow for calculation of sd and new weight vector
+    #So I abort at that point.
+    if (length(dneg) <=1){
+      return(arPLSresult)
+      break;}
+
+    m=mean(dneg)
+    s=sd(dneg)
+    w <- 1/(1+exp(2*(d-(-m+2*s))/s))
+
+    arPLSresult$last_ratio<-norm(wold-w, type = "2")/ norm(wold, type = "2")
+    if(verbose){
+      print(paste("Weight vector change ratio: ",arPLSresult$last_ratio))
+      plot(y)
+      lines(z,col='red')
+    }
+    if (arPLSresult$last_ratio < ratio){
+      return(arPLSresult)
+      break;}
+    wold<-w
+  }
+  return(arPLSresult)
+}
+
+
+#A2Data = read.table("C:/Users/corvini/OneDrive - SPC/Documents/baselineARPLss/data/Abelsonite.txt",header=FALSE,stringsAsFactors=FALSE)
+
+
+# Generate wavenumbers
+#wavenumbers <- seq(0, 1000, length.out = 1000)
+#raman_spectra <- 100*exp(-((wavenumbers-300)/15)^2) + 200*exp(-((wavenumbers-750)/30)^2) #+ 100*exp(-((wavenumbers-800)/15)^2)
+#result<-baseline_correction(raman_spectra,lambda=0.1,verbose=TRUE)
